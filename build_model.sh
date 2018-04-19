990c411459856862ed96a7383018eeef3ac4365b
Xingyao Huang <xhuang17@ncsu.edu>
2018-04-18 20:47:12 -0400

    build model bash

--- build_model.sh
+++ build_model.sh
@@ -18,19 +18,44 @@ web_host_ip=$2
 echo "Start building resource model..."
 
 # control cpu cgroup in the server
-for i in {3..9..2}
+for i in {1..99..1}
   do
     echo "Test for performance under cgroup $i..."
-    command="
-    echo '${i}0000' > /cgroup/cpu/limitcpu/cpu.cfs_quota_us
-    cgclassify -g cpu:limitcpu $(pidof $app_name)
+    root_command="
+    echo 'mount {
+            cpuset  = /cgroup/cpuset;
+            cpu     = /cgroup/cpu;
+            cpuacct = /cgroup/cpuacct;
+            memory  = /cgroup/memory;
+            devices = /cgroup/devices;
+            freezer = /cgroup/freezer;
+            net_cls = /cgroup/net_cls;
+            blkio   = /cgroup/blkio;
+    }
+
+    group limitcpu{
+            cpu {
+                    cpu.cfs_quota_us = ${i}0000;
+                    cpu.cfs_period_us = 1000000;
+            }
+    }' > /etc/cgconfig.conf
     "
-    ssh "root@$web_host_ip" "$command" >> /home/centos/resource_model/modeling_remote_log
-    
+    ssh "root@$web_host_ip" "$root_command" >> /home/centos/resource_model/res_model_log
+
+    user_command="
+    sudo service cgconfig restart
+    sudo cgclassify -g cpu:limitcpu \$(pidof $app_name)
+    "
+    ssh "centos@$web_host_ip" "$user_command" >> /home/centos/resource_model/res_model_log
+
     perf_path=$(make emulator | awk 'NR==4{ print $4 ; exit }')
     slo=$(cat /home/centos/RUBiS/${perf_path}perf.html | grep '<TR><TD><div align=left><B>Total</div></B><TD><div align=right>' | egrep -o [0-9]+ | sed -n '10 p')
     echo "$i $slo" >> /home/centos/resource_model/res_data
-  done
+    if [ "$slo" -lt 50 ]
+      then
+        break
+      fi
+    done
 
 # build resource model
 cd /home/centos/resource_model
